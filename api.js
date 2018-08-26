ab5a750e49d63414457c77d929d68c2a55a4df52
romeelkhan <romeel_k@yahoo.co.uk>
2018-08-26 11:01:18 +0100

    Angular node sample app

---
+++ api.js
@@ -0,0 +1,23 @@
+//By me
+var express = require('express')
+var bodyParser = require('body-parser')
+var Post = require('./models/post')
+
+var app = express()
+app.use(bodyParser.json())
+
+app.get('/', function (req, res) {
+  res.sendfile('layouts/index.html')
+  console.log("sending index.html")
+})
+
+
+app.use(require('./controllers/api/posts'))
+app.use( require('./controllers/static'))
+app.use(function(req, res) {
+        res.sendfile('layouts/404.html', 404);
+        });
+
+app.listen(3000, function () {
+  console.log('Server listening on', 3000)
+})
