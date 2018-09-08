8ee64fbb5efd23747dcbd2a8658ac20d5c78b4c1
Romeel Khan <romeel.khan@rackspace.co.uk>
2018-09-09 00:08:31 +0100

    added conditional for checking if running in container

--- db.js
+++ db.js
@@ -1,5 +1,19 @@
 var mongoose = require('mongoose')
-mongoose.connect('mongodb://mongo:27017/social', function () {
-  console.log('mongodb connected')
-})
+var isDocker = require('is-docker')
+
+if (isDocker()) {
+  console.log('Running inside a Docker container');
+
+  mongoose.connect('mongodb://mongo:27017/social', function () {
+    console.log('mongodb connected')
+  })
+}
+else
+{
+  console.log("Running outside container! connecting to localhost")
+  mongoose.connect('mongodb://localhost:27017/social', function () {
+    console.log('mongodb connected')
+  })
+}
+
 module.exports = mongoose
