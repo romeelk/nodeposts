a9123f829db3bf16f795223f8b89654569018c91
Romeel Khan <romeel.khan@rackspace.co.uk>
2018-09-08 23:11:57 +0100

    Added docker compose file

--- db.js
+++ db.js
@@ -1,5 +1,5 @@
 var mongoose = require('mongoose')
-mongoose.connect('mongodb://localhost:27017/social', function () {
+mongoose.connect('mongodb://mongo:27017/social', function () {
   console.log('mongodb connected')
 })
 module.exports = mongoose
