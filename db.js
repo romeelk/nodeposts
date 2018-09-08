018074b9ca5440b8e6d68d58c8f9d548765bef53
Romeel Khan <romeel.khan@rackspace.co.uk>
2018-09-09 00:19:40 +0100

    added variable for docker host

--- db.js
+++ db.js
@@ -1,17 +1,21 @@
 var mongoose = require('mongoose')
 var isDocker = require('is-docker')
 
+var mongoHost =  isDocker() ? "mongo" : "localhost";
+
+
+
 if (isDocker()) {
   console.log('Running inside a Docker container');
 
-  mongoose.connect('mongodb://mongo:27017/social', function () {
+  mongoose.connect('mongodb://'+mongoHost+':27017/social', function () {
     console.log('mongodb connected')
   })
 }
 else
 {
   console.log("Running outside container! connecting to localhost")
-  mongoose.connect('mongodb://localhost:27017/social', function () {
+  mongoose.connect('mongodb://'+mongoHost+':27017/social', function () {
     console.log('mongodb connected')
   })
 }
